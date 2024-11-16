53f310c38f78b4ab9ce22aeaf710803b6e5f36e5
Gregory2786 <114622096+Gregory2786@users.noreply.github.com>
2024-11-15 21:45:34 -0300

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        Link para o meu GitHub &#128073; 
+        <a href="https://github.com/Gregory2786">
+            Gregory Almeida Silva
+      </div >
+    );
+  }
